851026d7058870e5ae55b8f57f0f7f97706c303d
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-07 11:51:05 -0700

    first test is passing

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,9 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const sum = a + b;
+    const str = `The sum of ${a} and ${b} is ${sum}.`;
+    return [sum, str];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -26,7 +28,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
