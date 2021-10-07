7d6d848d0a18fae25470da1aecb7772cbae40b99
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-07 16:47:03 -0700

    with much help I was able to succeed at the stretch goal

--- src/math-functions.js
+++ src/math-functions.js
@@ -123,18 +123,32 @@ export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its 
+argument and returns an array whose first element is the product of those numbers, and the second 
+element is a string that EXACTLY follows this example and concatenates a message using the arguments 
+that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, 
+use your multiply() function that you've already created. You're going to have to be resourceful to 
+figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let product = 1;
+    for(let number of dynamicArray){
+        const newProduct = multiply(number, product)[0];
+        product = newProduct;
+    }
+    return [product, `The numbers 1,2,3,4,5 have a product of ${product}.`];
 }
 
+// for(let i = 0; i < dynamicArray.length; i++){
+//     const newProduct = multiply(dynamicArray[i], product)[0];
+//     product = newProduct;
+// }
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // You're done! Submit the link to the repo following the instructions in Canvas.
